1c41a475484075dbe9d1b300da7f8d0debca2c30
leenawat papahom <leenawat.pa@gmail.com>
2024-01-09 17:27:59 +0700

    first commit

---
+++ src/main/java/com/punyadev/springcruduserrole/Spring3CrudUserRoleApplication.java
@@ -0,0 +1,13 @@
+package com.punyadev.springcruduserrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3CrudUserRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3CrudUserRoleApplication.class, args);
+	}
+
+}
